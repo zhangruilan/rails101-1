aa8db4631f6214bf1e1ca93dc0fba8bf4ec43eca
张瑞兰 <zhangruilan007@gmail.com>
2017-01-03 23:32:22 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+         has_many :groups
          has_many :group_relationships
          has_many :partcipated_groups, :through => :group_relationships, :source => :group
          has_many :posts
          def is_member_of?(group)
            partcipated_groups.include?(group)
          end
-         
+
+         def join!(group)
+           partcipated_groups << group
+         end
+
+         def quit!(group)
+           partcipated_groups.delete(group)
+         end
+
 end
